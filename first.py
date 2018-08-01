46eda0c4ee36634b28b3059db86643ea48fc8c68
Alex <aleksandr-removich@protonmail.com>
2018-08-01 02:04:42 -0400

    Adding good/bad word variables

--- first.py
+++ first.py
@@ -1,7 +1,21 @@
+#Analyze the President's tweets to determine if they are presidential
 tweet_string = "Thanks to the historic TAX CUTS that I signed into law, your paychecks are going way UP, your taxes are going way DOWN, and America is once again OPEN FOR BUSINESS!"
 tweet_words = tweet_string.split()
 number_of_words = len(tweet_words)
+
+number_of_good_words = 0
+number_of_bad_words = 0
+
+good_words = ["Thanks", "historic", "paychecks"]
+bad_words = ["taxes"]
+
+
 print (tweet_words)
-print("Number of words in this tweet is: + str(number_of_words))
+print ("Number of words in this tweet is: " + str(number_of_words))
 
 #Iterate through the words in the tweet tweet_string
+print ("Words in the tweet are:")
+for w in tweet_words:
+    len_of_w = len(w)
+    print("number of letters in " + w + " is " + str(len_of_w) )
+print ("End of the words in the tweet")
