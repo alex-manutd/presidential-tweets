e410ed78d7e4dbeb42fb3ad27cebb039b734e8a1
Alex <aleksandr-removich@protonmail.com>
2018-08-01 23:00:11 -0400

    Stage 2 updates to analyze words for good or bad

--- first.py
+++ first.py
@@ -9,13 +9,28 @@ number_of_bad_words = 0
 good_words = ["Thanks", "historic", "paychecks"]
 bad_words = ["taxes"]
 
+# Iterate through the words in the tweet tweet_string
+for w in tweet_words:
+        print(w)
+        if w in good_words:
+            number_of_good_words += 1
+        elif w in bad_words:
+            number_of_bad_words += 1
 
-print (tweet_words)
-print ("Number of words in this tweet is: " + str(number_of_words))
+print ("There are " + str(number_of_good_words) + " good words in this tweet")
+print ("There are " + str(number_of_bad_words) + " bad words in this tweet")
+
+if number_of_good_words > number_of_bad_words:
+    print ("What a presidential thing to say! HUGE!")
+else:
+    print ("Surely you are joking, Mr. Trump! SAD!")
+
+#print (tweet_words)
+#print ("Number of words in this tweet is: " + #str(number_of_words))
 
 #Iterate through the words in the tweet tweet_string
-print ("Words in the tweet are:")
-for w in tweet_words:
-    len_of_w = len(w)
-    print("number of letters in " + w + " is " + str(len_of_w) )
-print ("End of the words in the tweet")
+#print ("Words in the tweet are:")
+#for w in tweet_words:
+#    len_of_w = len(w)
+#    print("number of letters in " + w + " is " + #str(len_of_w) )
+#print ("End of the words in the tweet")
